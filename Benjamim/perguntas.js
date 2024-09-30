16bdf663cc180b140f22b167955ec6f83904a6e7
benjamim21212 <144247200+benjamim21212@users.noreply.github.com>
2024-09-30 07:45:02 -0300

    Add files via upload

---
+++ Benjamim/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'O que é Corinthians?',
+    'O Corinthians é um estado perfeito de amor e paixão, o maior clube do mundo!'
+)
+
+criaCartao(
+    'Rival do Corinthians',
+    'O que é Palmeiras?',
+    'O palmeiras é uma bosta, um time de porcos imundos que não tem mundial.'
+)
+
+criaCartao(
+    'Filho do Corinthians',
+    'O que é São Paulo?',
+    'O São Paulo é o maior freguês do Corinthians, o eterno filho do todo poderoso Timão'
+)
+
+criaCartao(
+    'Maior torcida',
+    'O que é a fiel?',
+    'Simplismente a torcida mais louca e fanática do mundo'
+)
